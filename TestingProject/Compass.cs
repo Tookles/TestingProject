00d73f8518bbd078ae225d1fa322a9c6434ee050
Tookey <rachel.tookey@gmail.com>
2024-11-18 10:22:41 +0000

    Project creation

---
+++ TestingProject/Compass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingProject
+{
+    internal class Compass
+    {
+        public Point Point { get; set; }
+
+        public Point Rotate(Point NewPoint, Direction NewDirection)
+        {
+            return Point.South; 
+        }
+    }
+}
